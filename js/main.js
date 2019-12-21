b766c13b639b5a92cc3b18fa423a0027e9aa55f7
VisualYuki <comedy951@yandex.ru>
2019-12-21 10:39:10 +0300

    10:39 AM

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-$(function() {
+$(document).ready(function() {
+  $(".slider__items").slick({
+    dots: true
+  });
+
+  let time = document.querySelectorAll(".slick-dots button");
+
+  time[0].innerText = "";
+  time[1].innerText = "";
+  time[2].innerText = "";
+
   $(".menu-icon").on("click", function() {
     $(".menu__inner").slideToggle();
   });
